fa6830ac8d8db8626ef585c6367f497edd4eb71c
Albin Ryberg <albin@rybergs.net>
2025-10-31 12:24:59 +0100

    Auth folder added

--- backend/main.py
+++ backend/main.py
@@ -14,3 +14,14 @@ def read_root():
 def read_posts(db: Session = Depends(get_db)):
     posts = db.query(Post).all()
     return posts
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id == post_id).first()
+    if not post:
+        raise HTTPExeption(status_code=404, detail="Post not found")
+    return post
+
+@app.post("/auth/register")
+def register(username: string, email: string, password: string, db: Session = Depends(get_db)):
+    print("register ping")
